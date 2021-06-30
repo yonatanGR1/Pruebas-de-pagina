dc341006194b3ac69d136f364dd0271bb3dc5bdd
yonatanGR1 <86740493+yonatanGR1@users.noreply.github.com>
2021-06-30 12:10:51 -0600

    Add files via upload

---
+++ JS/java.js
@@ -0,0 +1,14 @@
+function Menu(){
+    document.getElementById("contenedor-menu").style.visibility = "visible";
+    document.getElementById("contenedor-menu").style.opacity = "1";
+}
+
+function cerrar(){
+    document.getElementById("contenedor-menu").style.visibility = "hidden";
+    document.getElementById("contenedor-menu").style.opacity = "0";
+}
+
+function ITE(){
+    document.getElementById("ITE").style.visibility = "visible";
+    document.getElementById("Ocultar").style.visibility = "hidden";
+}
